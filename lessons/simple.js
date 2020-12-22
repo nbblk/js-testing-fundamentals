b93311ebffa8d7d5b38a26c60cb69aaa40c07f7c
blks <nbblks@gmail.com>
2020-12-22 14:43:52 +0900

    fixup! [Course] m11-3 basic concept of testing and assertion framework/library

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,20 +1,26 @@
 const {sum, subtract} = require('../math')
 
+// * The testing framework is to throw practical error messages for a developer
+// when the result is not equal to the expected.
 let result, expected
 
 result = sum(3, 7)
 expected = 10
+// These statements can be abstracted like below:
+// if (expect !== result) {
+//   throw new Error(`${result} is not equal to ${expected}`);
+// }
 expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
 expect(result).toBe(expected)
 
-// mini assertion library
+// * The assertion library is a set of function that returns object of assertion, attatched to the function for expectation.
 function expect(actual) {
   // returns an object which has functions for assertion
   return {
-//  You don't need to specify the 'function' keyword when defining functions inside objects since es6.
+//  * You don't need to specify the 'function' keyword when defining functions inside objects since es6.
 //  Before:
 //    toBe: function toBe(expected) {}
 //    toEqual: function toEqual(expected) {}
