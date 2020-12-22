610c5c8168353c48471eec6456327e344cde00b1
blks <nbblks@gmail.com>
2020-12-22 14:37:19 +0900

    [Course] m11-3 basic concept of testing and assertion framework/library

--- lessons/simple.js
+++ lessons/simple.js
@@ -4,12 +4,28 @@ let result, expected
 
 result = sum(3, 7)
 expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+// mini assertion library
+function expect(actual) {
+  // returns an object which has functions for assertion
+  return {
+//  You don't need to specify the 'function' keyword when defining functions inside objects since es6.
+//  Before:
+//    toBe: function toBe(expected) {}
+//    toEqual: function toEqual(expected) {}
+//    toBeGreaterThan: function toBeGreaterThan(expected) {}
+//  After:
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {}
+  }
 }
